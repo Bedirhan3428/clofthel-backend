8864a47ef88c790fd48e6d43d2c233ab62d0bf23
Bedirhan Imer <abimer2350@gmail.com>
2026-06-01 23:02:02 +0300

    deploy: update API_BASE_URL to production domain

--- src/constants/config.js
+++ src/constants/config.js
@@ -4,9 +4,7 @@
  */
 
 // Backend API base URL
-// ⚠️ Kendi yerel IP adresinizi buraya yazın!
-// Windows'ta CMD'ye "ipconfig" yazarak öğrenebilirsiniz.
-export const API_BASE_URL = 'http://192.168.1.10:5000/api';
+export const API_BASE_URL = 'https://api.clofthel.com.tr/api';
 
 // AniList GraphQL endpoint
 export const ANILIST_API_URL = 'https://graphql.anilist.co';
